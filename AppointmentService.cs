3d5c072f40a77c173b8945eeb3e4f994f53b8e2a
Reyamlsh <153020739+Reyamlsh@users.noreply.github.com>
2023-12-10 22:10:04 +0200

    Add files via upload

---
+++ AppointmentService.cs
@@ -0,0 +1,69 @@
+﻿using Core.Domain;
+using Core.Repository;
+using Core.Service;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class AppointmentService : BaseEntityService<Appointment>, IAppointmentService
+    {
+        public AppointmentService(IRepository<Appointment> repository) : base(repository)
+        {
+        }
+
+        public bool CancelAppointment(int ID)
+        {
+            Appointment app = _repository.GetByID(ID);
+            app.status = AppointmentStatus.Canceled;
+            return true;
+        }
+
+        public bool ConfirmCheckUp(int ID)
+        {
+            Appointment app = _repository.GetByID(ID);
+            app.status = AppointmentStatus.Completed;
+            return true;
+        }
+
+        public Dictionary<string, int> NumOfRequests()
+        {
+            IEnumerable<Appointment> app = _repository.GetAll();
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            int numOfAllRequests = app.Count();
+            int numOfPendingRequests = 0;
+            int numOfCompletedRequests = 0;
+            int numOfCanceledRequests = 0;
+
+            foreach (Appointment appointment in app) 
+            {
+                if(appointment.status == AppointmentStatus.Canceled)
+                {
+                    numOfCanceledRequests++;
+                }
+
+                if (appointment.status == AppointmentStatus.Pending)
+                {
+                    numOfPendingRequests++;
+                }
+
+                if (appointment.status == AppointmentStatus.Completed)
+                {
+                    numOfCompletedRequests++;
+                }
+            }
+
+            result.Add("#Requests: ", numOfAllRequests);
+            result.Add("#PendingRequests: ", numOfPendingRequests);
+            result.Add("#CompletedRequests: ", numOfCompletedRequests);
+            result.Add("#CanceledRequests: ", numOfCanceledRequests);
+
+            return result;
+        }
+    }
+}
---
+++ SpecializationService.cs
@@ -0,0 +1,46 @@
+﻿using Core.Domain;
+using Core.Repository;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class SpecializationService : BaseEntityService<Specialization>, ISpecializationService
+    {
+        public SpecializationService(IRepository<Specialization> repository) : base(repository)
+        {
+        }
+
+        public IEnumerable<(string, int)> Top5Specializations()
+        {
+            IEnumerable<Doctor> docs = (IEnumerable<Doctor>)_repository.GetAll();
+            IEnumerable<Specialization> specs = _repository.GetAll();
+            IEnumerable<Doctor> top5 = new List<Doctor>();
+
+            for (int i = 0; i < docs.Count(); i++)
+            {
+                foreach (var spec in specs)
+                {
+                    if (docs.ElementAt(i).specialization.ID == spec.ID)
+                    {
+                        if (docs.ElementAt(i).requests.Count() > docs.ElementAt(i + 1).requests.Count())
+                        {
+                            top5.Append(docs.ElementAt(i));
+                        }
+                    }
+                }
+
+            }
+
+            var top5specializations = top5.OrderByDescending(d => d.requests.Count()).Take(5)
+                .Select(d => (d.specialization.name, d.requests.Count()));
+
+            return top5specializations;
+        }
+    }
+}
